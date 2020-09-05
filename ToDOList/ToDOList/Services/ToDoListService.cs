4189096ce401f2664dd3fb986c065e42b7f590f9
HsuLei <hsulei03@gmail.com>
2020-09-05 22:24:46 +0800

    FIX

---
+++ ToDOList/ToDOList/Services/ToDoListService.cs
@@ -0,0 +1,45 @@
+﻿using DataLibrary.DB_Models;
+using DataLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDOList.ViewModel;
+
+namespace ToDOList.Services
+{
+    public class ToDoListService
+    {
+        public OperationResult AddJobs(ItemsViewModel items)
+        {
+            var result = new OperationResult();
+            try
+            {
+                ToDoContactsModel context = new ToDoContactsModel();
+
+                var repository = new ToDoRepository<WorkDetail>(context);
+
+                WorkDetail entity = new WorkDetail()
+                {
+                    WorkContent = items.WorkContent,
+                    Important = items.IsImportant ? "Y" : "N",
+                    PlanDate = DateTime.Now,
+                    CreationDate = DateTime.Now,
+                    LastUpdataDate = DateTime.Now
+                };
+
+                repository.Create(entity);
+                context.SaveChanges();
+                result.IsSuccessful = true;
+            }
+            catch(Exception ex)
+            {
+                result.IsSuccessful = false;
+                result.exception = ex;
+            }
+
+            return result;
+        }
+    }
+}
